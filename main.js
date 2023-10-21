6cb3f9321e98be6545551fbe6faccbfaee61d46f
abdulloh452 <141325876+abdulloh452@users.noreply.github.com>
2023-10-21 16:04:00 +0500

    text1
    asdf

---
+++ main.js
@@ -0,0 +1,9 @@
+
+let text2 = document.querySelector('.text2')
+let h1 = document.querySelector("h1")
+let p = document.querySelector("p") 
+let plus = document.querySelector(".plus")
+let minus = document.querySelector(".minus")
+
+
+h1.innerHTML. = "My To Do List"
